0bb324bef0e1f72c81fa89226860e32b76d4148e
Andrea <36511176+andrembr@users.noreply.github.com>
2024-03-14 21:21:32 +0100

    Update simulate_examples.R

--- sim_examples/simulate_examples.R
+++ sim_examples/simulate_examples.R
@@ -1,16 +1,17 @@
 library(MASS)
 library(mvnfast)
 
-simulate_LMM <- function(seed=NULL,p,k,ns,tau=1,sigmab,sigmae,setstype,
-                         lineartime=T,randomslope=F){
+simulate_LMM <- function(seed = NULL, p, k, ns, tau = 1, sigmab, sigmae, setstype,
+                         lineartime = T, randomslope = F){
   # p: Number of covariates
   # k: Number of time points
   # ns: Number of subjects
   # sigmab: standard deviation of random effects
-  # sigmae: standard deviation of the errors
-  # typeset: what are the true active sets of main effects and interactions?
-  # randint: is the model a random intercept model with linear effect of time (ex. 1 and 2)? T/F
-  # tau: the autocorrelation parameter for the covariates (tau=1 for ex. 1, tau=0.8 for ex. 2)
+  # sigmae: standard deviation of the noise
+  # typeset: what are the true active sets of main effects and interactions? either overlapping, disjoint or identical
+  # lineartime: linear effect of time T/F (T for ex. 1 and 2)
+  # randomslope: random intercept or random intercept + time slope (T for ex2)
+  # tau: the autocorrelation parameter for the covariates (tau=1 for ex. 1 and ex. 3, tau=0.8 for ex. 2)
   
   set.seed(seed)
   n <- ns*k # Total number of observations
@@ -64,7 +65,7 @@ simulate_LMM <- function(seed=NULL,p,k,ns,tau=1,sigmab,sigmae,setstype,
       y[i] <- 0.2*t[i] + x[i,]%*%beta0 + t[i]*x[i,]%*%alpha0
     }
     y <- y + Zb + rnorm(n,mean=0,sd=sigmae) # Add random effects + random noise
-    data <- list(y = y,x = x, t = t, id = as.factor(id), A0 = A, I0 = I)
+    data <- list(y = y, x = x, t = t, id = as.factor(id), A0 = A, I0 = I)
   }
   
   else if (lineartime == F){            # not linear effect of time for ex 3
@@ -89,9 +90,9 @@ simulate_LMM <- function(seed=NULL,p,k,ns,tau=1,sigmab,sigmae,setstype,
       y[i] <- x[i,]%*%beta+ti%*%alpha+t(x[i,])%*%Omega%*%ti
     }
     y <- y + Zb + rnorm(n,mean=0,sd=sigmae)
-    data <- list(y = y,x = x,
+    data <- list(y = y, x = x,
                  time = as.factor(time),
-                 id = as.factor(id),A0=A,I0=I)
+                 id = as.factor(id), A0 = A, I0 = I)
   }
   return(data)
 }
